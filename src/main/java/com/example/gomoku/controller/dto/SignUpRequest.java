309fed865e6f00f02fc89238e69d55a5daff88f1
Albatross3 <superstring77@gmail.com>
2022-12-16 13:02:26 +0900

    feat: 로그인/회원가입 화면

--- src/main/java/com/example/gomoku/controller/dto/SignUpRequest.java
+++ src/main/java/com/example/gomoku/controller/dto/SignUpRequest.java
@@ -1,25 +1,4 @@
 package com.example.gomoku.controller.dto;
 
-public class SignUpRequest {
-    String email;
-    String password;
-    String name;
-
-    public SignUpRequest(String email, String password, String name) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
+public record SignUpRequest(String email, String password, String name){
 }
